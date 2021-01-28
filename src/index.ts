7f146dab08858c4b3b74b0afd7faf7e5dfc3eddd
pikokr <parangee9706@gmail.com>
2021-01-28 09:59:31 +0900

    feat: init bot

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { CommandClient } from "@pikostudio/command.ts";
+import { ShardingManager } from "discord.js";
+
+const config = require("../config.json");
+
+declare module "discord.js" {
+  interface Client {
+    config: any;
+  }
+}
+
+if (process.env.SHARDING_MANAGER) {
+  const client = new CommandClient({
+    watch: true,
+    owners: "auto",
+    commandHandler: {
+      prefix: config.prefix,
+    },
+    currentDir: __dirname,
+  });
+
+  client.config = config;
+  client.login(config.token);
+} else {
+  const manager = new ShardingManager(__filename, {
+    execArgv: __filename.endsWith(".ts") ? ["-r", "ts-node/register"] : [],
+    token: config.token,
+  });
+  manager.spawn();
+}
